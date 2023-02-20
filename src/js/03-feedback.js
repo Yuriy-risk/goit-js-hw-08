b531362c49d1e7c207078ae569bf65faa19743f7
Yuriy-risk <111949781+Yuriy-risk@users.noreply.github.com>
2023-02-20 20:29:23 +0200

    new init

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -30,6 +30,9 @@ function onFormSubmit(e) {
   console.log(formData);
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  formData.email = '';
+  formData.message = '';
+
 }
 
 function entryFormInput() {
